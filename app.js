3842570156546d3bd3b5793420aa99bd6e700ebf
Stanislav <gamongara@gmail.com>
2022-06-01 13:54:50 +0300

    task bug

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
 const app = express()
-app.use(express.json({limit: '50mb', extended: true }));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(bodyParser.json({limit: "500mb"}));
+app.use(bodyParser.urlencoded({limit: "500mb", extended: true, parameterLimit:50000}));
 
 const PORT = config.get('port') || 5000
 
